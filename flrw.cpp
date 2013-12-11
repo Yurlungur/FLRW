00a0281904709cfd3b6dda15dc7b13b83fbbf6e0
Jonah Miller <DrLoco3000@gmail.com>
2013-12-11 02:29:50 -0500

    new file:   flrw.cpp 	new file:   flrw.hpp

---
+++ flrw.cpp
@@ -0,0 +1,19 @@
+// flrw.cpp
+
+// Author: Jonah Miller (jonah.maxwell.miller@gmail.com)
+// Time-stamp: <2013-12-11 02:26:12 (jonah)>
+
+// This is the implementation of my program to numerically explore
+// Friedmann-Lemaitre-Robertson-Walker metrics.
+
+// For background, see the included pdf or latex file. For usage, see
+// the readme or the header file.
+
+// Dependencies
+// ----------------------------------------------------------------------
+// This program depends on the RKF45 package I wrote, which is an
+// implementation of a fourth-order Runge-Kutte Fehlberg integration
+// scheme. If you don't have it, you can find it here:
+// https://github.com/Yurlungur/runge_kutta
+// ----------------------------------------------------------------------
+
---
+++ flrw.hpp
@@ -0,0 +1,33 @@
+// flrw.hpp
+
+// Author: Jonah Miller (jonah.maxwell.miller@gmail.com)\
+// Time-stamp: <2013-12-11 02:29:38 (jonah)>
+
+// This is the prototype and header file for the program that uses
+// Runge-Kutta methods to numerically explore
+// Friedmann-Lemaitre-Robertson-Walker metrics.
+
+// Background
+// ----------------------------------------------------------------------
+// See the included pdf or latex file.
+// ----------------------------------------------------------------------
+
+
+// Dependencies
+// ----------------------------------------------------------------------
+// This program depends on the RKF45 package I wrote, which is an
+// implementation of a fourth-order Runge-Kutte Fehlberg integration
+// scheme. If you don't have it, you can find it here:
+// https://github.com/Yurlungur/runge_kutta
+// ----------------------------------------------------------------------
+
+
+// Usage
+// ----------------------------------------------------------------------
+// STUB
+// ----------------------------------------------------------------------
+
+
+// Include guard
+#pragma once
+
